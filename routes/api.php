8e3fabfac54bdc8d3280f057af9e2d8158b647ae
milica5kovic <mpetkovic452@gmail.com>
2024-06-06 22:15:45 +0200

    a

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use app\Http\Controllers\StudentController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
